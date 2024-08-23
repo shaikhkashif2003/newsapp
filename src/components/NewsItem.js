01734620a8b4a1d55e239b6955554bf824a2de81
shaikhkashif2003 <119797443+shaikhkashif2003@users.noreply.github.com>
2024-08-23 23:44:55 +0530

    "Added dark mode toggle to NavBar, updated News component to fetch API data with pagination, and modified NewsItem to add rel='noreferrer' to Read More link."

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+      let {title, description, imageUrl, url} = this.props;
     return (
-      <div>
-        This is a news Item
+      <div className="my-3" >
+        <div className="card" style={{width: "18rem"}}>
+            <img src={!imageUrl?"https://techcrunch.com/wp-content/uploads/2024/05/wildfire_drone_web.jpg?w=900":imageUrl} className="card-img-top" alt="..."/>
+            <div className="card-body">
+                <h5 className="card-title">{title}...</h5>
+                <p className="card-text">{description}...</p>
+                <a href={url} target='_blank' className="btn btn-sm btn-primary">Read More</a>
+            </div>
+            </div>
       </div>
     )
   }
